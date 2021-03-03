1a6e24263e743b0f7e0ba89f87a09039a593c003
Shoriful Islam <53949222+MeSagor@users.noreply.github.com>
2021-03-03 14:22:12 +0600

    Add files via upload
    Adding my very first webpage...

---
+++ MyScripts.js
@@ -0,0 +1,18 @@
+function facebook(){
+    window.top.location.href='https://www.facebook.com/me.ssagor';
+}
+function linkedin(){
+    window.top.location.href='https://www.linkedin.com/in/shoriful-islam-3940411bb/';
+}
+function twitter(){
+    window.top.location.href='https://twitter.com/ItsMeSagor';
+}
+function insta(){
+    window.alert("I Don't Have Any Account yet!");
+}
+function reachMe(){
+    window.alert("Gmail: me.sshorifulislam@gmail.com");
+}
+function noPages(){
+    window.top.location.href='Weather/weatherpage.html';
+}
